dc637c5793dc7c169aad4c9f27bc7c214f4c6194
Ripon Sk <riponsk477@gmail.com>
2022-10-20 22:50:54 +0530

    commit all files

--- videoPlay_In_Swift/ViewController.swift
+++ videoPlay_In_Swift/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
-
+import AVKit
+import AVFoundation
 class ViewController: UIViewController {
-
+    var player:AVPlayer?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        playvideo()
+       NotificationCenter.default.addObserver(self, selector: #selector(resignactive), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(playwhileactive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(playbackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+    }
+    @objc func playbackground(){
+        player?.play()
+    }
+    @objc func playwhileactive(){
+        player?.play()
+    }
+    @objc func resignactive(){
+        player?.pause()
     }
 
-
+    func playvideo(){
+        let url = Bundle.main.url(forResource: "khermangdi", withExtension: "mp4")
+        player = AVPlayer(url: url!)
+        let playerlayer = AVPlayerLayer(player: player)
+        playerlayer.frame = self.view.bounds
+        player?.play()
+        self.view.layer.addSublayer(playerlayer)
+    }
 }
 
